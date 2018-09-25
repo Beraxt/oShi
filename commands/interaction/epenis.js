4ee502f5153d1c22abf3e29e741b2c28b32c7739
ShAdOwInK <alexandredelaloydev@gmail.com>
2018-09-25 23:31:12 +0200

    ✨ add epenis command

---
+++ commands/interaction/epenis.js
@@ -0,0 +1,34 @@
+const { epenis } = require('../../config.json');
+
+module.exports = {
+	name: 'epenis',
+	description: 'Give a size of your e-penis o/',
+	aliases: ['penis', 'dick', 'pickle'],
+	usage: '|| @user',
+	type: 'interaction',
+	cooldown: 2,
+	args: false,
+	guild: false,
+	execute(message, args) {
+		const penisSize = Math.floor(Math.random() * Math.floor(20));
+
+		for (let i = 0; i < epenis.length; i++) {
+			if (penisSize <= epenis[i].size) {
+				const pic = {
+					color: 0x0099ff,
+					image: {
+						url: epenis[i].pic,
+					},
+				};
+				if (!message.mentions.users.size) {
+					message.channel.send('Your *e-penis* size is **' + penisSize + ' inches**, like a ' + epenis[i].animal);
+					return message.channel.send({ embed: pic });
+				}
+				else {
+					message.channel.send('The ' + args[0] + '\'s *e-penis* size is **' + penisSize + ' inches**, like a ' + epenis[i].animal);
+					return message.channel.send({ embed: pic });
+				}
+			}
+		}
+	},
+};
