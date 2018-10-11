0e5b1e66f01b06ba51726b569e4cd427699d6d5a
ShAdOwInK <alexandredelaloydev@gmail.com>
2018-10-11 16:31:21 +0200

    👌 update autoanswers to es6
     #20

--- commands/information/autoanswers.js
+++ commands/information/autoanswers.js
@@ -3,7 +3,7 @@ const { responses } = require('../../data.json');
 module.exports = {
 	name: 'autoanswers',
 	description: ':card_box: **|** Send a list of the auto answers of the bot ! ',
-	aliases: ['at', 'auto', 'answers', 'answer'],
+	aliases: ['aa', 'auto', 'answers', 'answer'],
 	usage: '',
 	type: 'information',
 	cooldown: 5,
@@ -11,7 +11,7 @@ module.exports = {
 	guild: false,
 	execute(message) {
 		const responsesArray = [];
-		responsesArray.push(responses.map(response => '`' + response.cited + '` ➤  *' + response.response + '*').join('\n'));
+		responsesArray.push(responses.map(response => `\`${response.cited} \` ➤  *${response.response}*`).join('\n'));
 		return message.channel.send(responsesArray);
 	},
 };
